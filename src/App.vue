4e92731db63c6dfc8ca120872a93b69e967ffa5a
MichaelKramerGuitar <michael.kramer.guitar@gmail.com>
2022-06-13 10:54:34 -0400

    deleted vue automated stuff

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,18 @@
 <template>
-<!--  <img alt="Vue logo" src="./assets/logo.png">-->
-<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
   <ComicBook></ComicBook>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
 import ComicBook from "@/components/ComicBook";
 
 export default {
   name: 'App',
   components: {
     ComicBook
-    //HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
